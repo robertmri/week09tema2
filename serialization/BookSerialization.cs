f2de67879cfb6828512b7100b4f0586304507a95
robert manole <robertmri95@gmail.com>
2019-12-10 11:57:49 +0200

    //

---
+++ serialization/BookSerialization.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Xml.Serialization;
+using java.awt.print;
+
+namespace serialization
+{
+    class BookSerialization
+    {
+        public static void BooksXML(List<Book> books)
+        {
+            
+            XmlSerializer xmls_books = new XmlSerializer(typeof(List<Book>));
+            TextWriter tw_books = new StreamWriter("Books.xml");
+            xmls_books.Serialize(tw_books, books);
+        }
+
+        public static void BooksJson(List<Book> books)
+        {
+            JsonSerializer jsons_books = new JsonSerializer();
+            using (StreamWriter jsonsw_books = new StreamWriter("Books.json"))
+            using (JsonWriter jw_books = new JsonTextWriter(jsonsw_books))
+            {
+                jsons_books.Serialize(jw_books, books);
+            }
+        }
+    }
+}
---
+++ serialization/Program.cs
@@ -0,0 +1,21 @@
+﻿using java.awt.print;
+using SummaryBookApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>();
+            books.Fill(BooksCrud.GetTop10PBooks());
+            BookSerialization.BooksJson(books);
+            BookSerialization.BooksXML(books);
+        }
+    }
+}
